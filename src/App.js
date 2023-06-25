42838b96a9adc648047fc3568de8a443c81f830b
RodrigoAgustin11 <rodrigo.agustin1105@gmail.com>
2023-06-25 19:18:09 -0300

    Se agrega el Proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <form onSubmit={calcularVocales}>
+        <p>Contar vocales de la palabra:
+           <input type="textarea" name="texto1" />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <input type="submit" value="mostrar" />
+        </p>
+      </form>
     </div>
   );
+}
+
+function calcularVocales(e) {
+e.preventDefault();
+let texto=e.target.texto1.value;
+let contarVocales=0;
+let vocales=['a','e','i','o','u','A','E','I','O','U'];
+for(let i=0;i<texto.length;i++){
+  for(let s=0;s<vocales.length;s++){
+    if(texto.charAt(i)==vocales[s]||texto.charAt(i)==vocales[s]){
+      contarVocales++;
+    }
+  }
+}
+alert('La cantidad de vocales es: '+contarVocales)
 }
 
 export default App;
